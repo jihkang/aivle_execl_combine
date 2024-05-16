cd497f7a05534905b90136d4c83b48731d6c1f57
jihkang <106561135+jihkang@users.noreply.github.com>
2024-05-16 15:49:39 +0900

    .

--- excel_combine.py
+++ excel_combine.py
@@ -1,78 +1,97 @@
 import os
 import pandas as pd
+import warnings
 from openFolder import scanDir
+from datetime import datetime
+from openpyxl.styles import Alignment, Font
+import openpyxl
+import openai
 
-
+warnings.filterwarnings('ignore')
 remove_header = ['번호', '상태', '본부', '반', '교육생', '교육상태', '검토담당자', '검토일시']
+ind = pd.read_excel('3기 과정 목록.xlsx', header=1, sheet_name=None)
+for key in ind.keys():
+	ind[key] = ind[key][['시작일자', '종료일자', '과정명']][:24]
 
 
 def openExcel(func):
-	def wrapper(*arg, **kwargs):
-		kwargs['data'] = scanDir()
-		result = func(**kwargs)
+	def wrapper():
+		result = func(scanDir())
 		return result
 	return wrapper
 
 
+def writeExcel(data):
+	with pd.ExcelWriter('dist/result.xlsx') as writer:
+		for key in data.keys():
+			if data[key] is None:
+				continue
+			data[key].reset_index(drop=True, inplace=True)
+			data[key].drop(index=0, axis=1)
+			data[key].insert(
+				loc=0,
+				column='번호',
+				value=data[key].index,
+			)
+			data[key].to_excel(writer, sheet_name=key, index=False)
+
+
+
 @openExcel
-def readExcel(**kwargs):
-	ai_data = None
-	dx_data = None
-	for excel in kwargs['data']:
-		excel_read = pd.read_excel(excel, header=1, sheet_name=None)
+def readExcel(rdata):
+	data = {
+		'AI': None,
+		'DX': None,
+	}
+	for excel in rdata:
+		excel_read = pd.read_excel(
+			excel,
+			header=1,
+			sheet_name=None,
+			engine="openpyxl"
+		)
 		excel_keys = excel_read.keys()
-		tmpvalue = excel.replace(f"{os.getcwd()}/excel/", '').replace('.xlsx', '')
-		value = tmpvalue.split('_')
-		print(value)
+		value = excel.replace(f"{os.getcwd()}/excel/", '').replace('.xlsx', '').split('_')
 		for key in excel_keys:
-			excel_read[key] = excel_read[key].drop(columns=remove_header, axis=1)
+			excel_read[key] = excel_read[key].drop(
+				columns=remove_header,
+				axis=1
+			)
 			excel_read[key].rename(columns={'답변': '튜터 답변'}, inplace=True)
 			excel_read[key].insert(
 				loc=4,
 				column='과목명',
 				value=value[1],
 			)
-			excel_read[key]['챗GPT 문의내용'] = ""
-			excel_read[key]['챗GPT답변'] = ""
-			excel_read[key]['답변 사용 가능 여부'] = ""
-			excel_read[key]['비고'] = ""
-			if value[0] == "AI":
-				if ai_data is None:
-					ai_data = excel_read[key]
-				else:
-					ai_data = pd.concat([ai_data, excel_read[key]])
-			elif value[0] == "DX":
-				if dx_data is None:
-					dx_data = excel_read[key]
-				else:
-					dx_data = pd.concat([dx_data, excel_read[key]])
-
-	with pd.ExcelWriter('dist/combine.xlsx') as writer:
-		ai_data.reset_index(drop=True, inplace=True)
-		ai_data.drop(index=0, axis=1)
-		ai_data.insert(
-			loc=0,
-			column='번호',
-			value=ai_data.index,
-		)
-		ai_data.to_excel(writer, sheet_name='AI', index=False)
-		dx_data.reset_index(drop=True, inplace=True)
-		dx_data.drop(index=0, axis=1)
-		dx_data.insert(
-			loc=0,
-			column='번호',
-			value=ai_data.index,
-		)
-		dx_data.to_excel(writer, sheet_name='DX', index=False)
-	# data.to_excel(
-	# 	os.getcwd() + '/dist/combined.xlsx',
-	# 	engine="openpyxl",
-	# 	index=False,
-	# )
+			excel_read[key]['챗GPT 문의내용'] = excel_read[key]["문의내용"]
+			excel_read[key]['챗GPT답변'] = excel_read[key]['답변 사용 가능 여부'] = excel_read[key]['비고'] = ""
+			if '미니프로젝트' in value[1] or '코딩마스터즈' in excel_read[key]['문의유형']:
+				excel_read[key]['비고'] = 0
+			else:
+				response = openai.Completion.create(
+                    engine="gpt-3.5-turbo-instruct",
+                    prompt=f"please answer each question: {excel_read[key]['챗GPT 문의내용']}",
+                    max_tokens=1000
+                )
+				exce_read[key]['챗GPT 답변'] = response.choices[0].text.strip()
+			if data[value[0]] is None:
+				data[value[0]] = excel_read[key]
+			else:
+				data[value[0]] = pd.concat([data[value[0]], excel_read[key]])
+	writeExcel(data)
 
 
 def main():
-	readExcel()
-
+	# readExcel()
+	# result =
+	# result['과정명'][1]
+	# print(result['과정명'])
+	# test = pd.read_excel(, sheet_name=None)
+	test = openpyxl.load_workbook('dist/result.xlsx')
+	for i in test.sheetnames:
+		for row in test[i]:
+			for cell in row:      
+				cell.alignment = Alignment(wrap_text=True)
+	test.save('dist/result.xlsx')	
 
 main()
